b3f725a9e527bab778d25fa61e2f25c7f14a70a0
jordan <sudo.sendmail.jordan@gmail.com>
2025-02-17 18:13:30 -0800

    Release: Update Format
    - add directory to path

--- release.sh
+++ release.sh
@@ -1,14 +1,10 @@
 ROOT=$PWD
 mkdir $ROOT/release
 
-$PWD/install.sh -a -d $PWD/release
+$PWD/install.sh -a -d $PWD/release # Build
 
-cd $ROOT/release
-tar -cJvf $ROOT/Win11.tar.xz *
+cd $ROOT/release; tar -cJvf $ROOT/Win11.tar.xz *; # Compress all
 
-for DIRECTORY in $ROOT/release/*; do
-    cd $DIRECTORY
-    tar -cJvf $DIRECTORY.tar.xz *
-done
+for DIRECTORY in *; do tar -cJvf $DIRECTORY.tar.xz $DIRECTORY; done # Compress one 
 
-mv $ROOT/Win11.tar.xz $ROOT/release
+mv $ROOT/Win11.tar.xz $ROOT/release 
